e2d73f6df7901f55bac97c1c75ad5c5610f4b3dd
Aleksandr Pyzh <pyzh98@gmail.com>
2019-02-14 18:19:35 +0200

    Added backend for login page

--- Messenger/Controllers/LoginController.cs
+++ Messenger/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Messenger.Models;
 
 namespace Messenger.Controllers
 {
@@ -12,6 +13,32 @@ namespace Messenger.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        public ActionResult UserLogin(string login, string pass)
+        {
+            Users user;
+            using (var context = new MessengerDBEntities())
+            {
+                if (context.Users.Any(u => u.UserName == login)) {
+                    user = context.Users.FirstOrDefault(u => u.UserName == login);
+                }else if (context.Users.Any(u => u.Email == login))
+                {
+                    user = context.Users.FirstOrDefault(u => u.Email == login);
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "User not found!";
+                    return View("Login");
+                }
+
+                var password = user.Password;
+
+                if (pass == password) return RedirectToAction("Index", "Home", new { id = user.Id });
+            }
+
+            ViewBag.ErrorMessage = "Password is incorrect!";
+            return View("Login");
         }
     }
 }
