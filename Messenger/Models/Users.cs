c24e4775efcbd1cd878f4c9ded303c5f4fa41e81
Aleksandr Pyzh <pyzh98@gmail.com>
2019-03-05 23:41:58 +0200

    Updated model

--- Messenger/Models/Users.cs
+++ Messenger/Models/Users.cs
@@ -11,23 +11,13 @@ namespace Messenger.Models
 {
     using System;
     using System.Collections.Generic;
-
+    
     public partial class Users
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Bio { get; set; }
-        public Nullable<System.DateTime> LastSeen { get; set; }
-        public bool IsOnline { get; set; }
+        public string LastSeen { get; set; }
         public string Contacts { get; set; }
         public string Password { get; set; }
-
-        public string NameToString()
-        {
-            return FirstName + " " + LastName;
-        }
     }
 }
